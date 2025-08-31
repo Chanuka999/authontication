c650ac2d5c658fca0048466d26c70b8a7b6b2fa3
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-01 01:24:13 +0530

    Add initial package.json for server setup with dependencies

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/user.js";
+import cors from "";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/auth", UserRoutes);
+
+const PORT = process.env.PORT;
+
+const connectDb = async () => {
+  try {
+    const MONGOURL = process.env.MONGOURL;
+    await mongoose.connect(MONGOURL);
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.log("Mongodb failed");
+  }
+};
+
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`server running on ${PORT}`);
+});
