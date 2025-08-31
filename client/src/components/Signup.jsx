901eecbd3f740ac896e197194c117bb71088c19a
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-01 01:23:33 +0530

    feat: add Signup component and main application structure

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handdleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/auth/signup", {
+        username,
+        email,
+        password,
+      })
+      .then((responce) => {
+        console.log(responce);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handdleSubmit}>
+        <h2>sign up</h2>
+        <label htmlFor="username">username</label>
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+
+        <label htmlFor="username">E mail</label>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="username">password</label>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
