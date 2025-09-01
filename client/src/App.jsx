adff9148be5efd50919cfccc303385e10bf9cc11
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-01 23:30:45 +0530

    feat: implement routing with BrowserRouter and add Login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
